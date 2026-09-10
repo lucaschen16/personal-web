955d2a9f407150750a6f17e928110ca90e8d7a01
yimingsato <yimingsato@gmail.com>
2026-09-10 16:54:38 -0400

    fix

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -11,30 +11,30 @@ export default function ExperiencePage() {
       <p>Looking for some.</p>
     </SectionCard>
   );
-  // return (
-  //   <SectionCard title="Experience">
-  //     <div className="space-y-10">
-  //       {experiences.map((experience) => (
-  //         <article key={experience.title}>
-  //           {experience.href ? (
-  //             <a
-  //               href={experience.href}
-  //               target="_blank"
-  //               rel="noreferrer"
-  //               className="link-hit-block"
-  //             >
-  //               <h2 className="text-lg font-bold">{experience.title}</h2>
-  //               <p className="mt-2">{experience.description}</p>
-  //             </a>
-  //           ) : (
-  //             <>
-  //               <h2 className="text-lg font-bold">{experience.title}</h2>
-  //               <p className="mt-2">{experience.description}</p>
-  //             </>
-  //           )}
-  //         </article>
-  //       ))}
-  //     </div>
-  //   </SectionCard>
-  // );
+  return (
+    <SectionCard title="Experience">
+      <div className="space-y-10">
+        {experiences.map((experience) => (
+          <article key={experience.title}>
+            {experience.href ? (
+              <a
+                href={experience.href}
+                target="_blank"
+                rel="noreferrer"
+                className="link-hit-block"
+              >
+                <h2 className="text-lg font-bold">{experience.title}</h2>
+                <p className="mt-2">{experience.description}</p>
+              </a>
+            ) : (
+              <>
+                <h2 className="text-lg font-bold">{experience.title}</h2>
+                <p className="mt-2">{experience.description}</p>
+              </>
+            )}
+          </article>
+        ))}
+      </div>
+    </SectionCard>
+  );
 }
